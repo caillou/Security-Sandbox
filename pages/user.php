4dad88c22ca003fb0f54b29e0b16e6fdb022394d
Pierre Spring <pierre.spring@caillou.ch>
2011-04-29 00:11:10 +0200

    removed the Content-Type "text/plain" as I usually have xdebug enabled

--- pages/user.php
+++ pages/user.php
@@ -2,7 +2,7 @@
 require '../lib/vendor/querypath/src/QueryPath/QueryPath.php';
 require '../conf/config.php';
 
-header("Content-Type: text/plain");
+//header("Content-Type: text/plain");
 
 // get the user info
 var_dump($db->query('select * from user where username = ' . $db->quote($_REQUEST['username']))->fetchObject());
