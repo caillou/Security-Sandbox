8f7acdb4987a47d38478558a47a169f121a96120
Pierre Spring <pierre.spring@caillou.ch>
2011-04-29 00:05:33 +0200

    Updated to latest OWASP Top Ten

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 header("Content-Type: text/plain");
 ?>
-short introduction to web security
+Short Introduction To Web Security
 ==================================
 
-owasp top 10
+OWASP Top 10
 ------------
-http://www.owasp.org/index.php/File:OWASP_T10_-_2010_rc1.pdf
+http://www.owasp.org/index.php/Category:OWASP_Top_Ten_Project
 
  1. Injection *
- 2. Cross Site Scripting (XSS) *
+ 2. Cross-Site Scripting (XSS) *
  3. Broken Authentication and Session Management
  4. Insecure Direct Object References *
- 5. Cross Site Request Forgery (CSRF) *
+ 5. Cross-Site Request Forgery (CSRF) *
  6. Security Misconfiguration (NEW)
- 7. Failure to Restrict URL Access *
- 8. Unvalidated Redirects and Forwards (NEW)
- 9. Insecure Cryptographic Storage
-10. Insufficient Transport Layer Protection
+ 7. Insecure Cryptographic Storage *
+ 8. Failure to Restrict URL Access *
+ 9. Insufficient Transport Layer Protection
+10. Unvalidated Redirects and Forwards (NEW)
 
 http://github.com/caillou/Security-Sandbox
